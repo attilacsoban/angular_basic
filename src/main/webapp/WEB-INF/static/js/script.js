b5e28e4935e3bc7454a25c38aa5478c0b74910e9
Csobban <csoban.attila@openminds.hu>
2015-06-15 10:21:05 +0200

    basic patterns finished

--- src/main/webapp/WEB-INF/static/js/script.js
+++ src/main/webapp/WEB-INF/static/js/script.js
@@ -1,34 +1,42 @@
-document.write("helloka");
 
-var work = function(){
-	console.log("working hard");
-};
-var doWork = function(f){
-	console.log("start working");
-	try {
-		f();
-	} catch (e) {
-		console.log(e);
-	}
-	console.log("working finished");
-};
-doWork(work);
-
-
-// object has data and method
-var createWorker = function (){
-	var task1 = function() {
-		console.log("task1");
-	}
-	var task2 = function() {
-		console.log("task2");
-	}
-	return {
-		job1: task1,
-		job2: task2
+(function(){
+	var work = function(){
+		console.log("working hard");
 	};
-};
+	var doWork = function(f){
+		console.log("start working");
+		try {
+			f();
+		} catch (e) {
+			console.log(e);
+		}
+		console.log("working finished");
+	};
+	doWork(work);
+	
+	
+	// object has data and method
+	var createWorker = function (){
+		var workCount = 0;
+		var task1 = function() {
+			workCount ++;
+			console.log("task1: " + workCount);
+		}
+		var task2 = function() {
+			workCount ++;
+			console.log("task2: "+workCount);
+		}
+		return {
+			job1: task1,
+			job2: task2
+		};
+	};
+	
+	var worker = createWorker();
+	
+	worker.job1();
+	worker.job2();
+	worker.job1();
+	worker.job2();
 
-var worker = createWorker();
-worker.job1();
-worker.job2();
+}());
