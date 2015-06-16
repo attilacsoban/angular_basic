c7aadfb64d2d266221c41bc3fdeb1fa031ffcc3a
Csobban <csoban.attila@openminds.hu>
2015-06-16 15:20:26 +0200

    own service good implemented but not yet working

---
+++ src/main/webapp/WEB-INF/static/js/github.js
@@ -0,0 +1,34 @@
+
+(function() {
+	
+	//design patter: function gets invoke return an object with api: revieling model design pattern
+	//angular will invoke this module/service, what i return represent the public api
+	//service has to get the user and the repos for that user, so it will need the basic $http service
+	var github = function($http) {
+		
+		//return promise here
+		var getUser = function(username) {
+			return $http.get("https://api.github.com/users/"+username).success(function(response) {
+				return response;
+			});
+		};
+		
+		var getRepos = function(user) {
+			return $http.get(user.repos_url).success(function(response) {
+				return response;
+			});
+		};		
+		
+		return {
+			getUser: getUser,
+			getRepos: getRepos			
+		};
+	};
+	
+	var myApp = angular.module("githubViewer");
+	
+	//register the service with angular, so the other components can use it
+	module.service("github",github);
+	
+	
+}());
